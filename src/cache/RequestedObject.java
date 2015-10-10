b3830c3a74b09797b3764ce8c511e6459dfc14a1
= <florian.thuin@student.uclouvain.be>
2015-10-10 22:31:00 +0200

    commenting RequestedObject

--- src/cache/RequestedObject.java
+++ src/cache/RequestedObject.java
@@ -5,41 +5,36 @@ public class RequestedObject {
 	private int size;
 	private int access_counter;
 
-  public RequestedObject(String name,int size)
-  {
-    this.name = name;
-    this.size = size;
-    this.access_counter = 0;
-  }
+    public RequestedObject(String name,int size) {
+        this.name = name;
+        this.size = size;
+        this.access_counter = 0;
+    }
 
-  public String getName()
-  {
-    return this.name;
-  }
+    public String getName() {
+        return this.name;
+    }
 
-  public int getAccessCounter()
-  {
-    return this.access_counter;
-  }
+    public int getAccessCounter() {
+        return this.access_counter;
+    }
 
-  public void incrementCounter()
-  {
-    this.access_counter++;
-  }
-  
-  public int getSize()
-  {
-    return this.size;
-  }
+    public void incrementCounter() {
+        this.access_counter++;
+    }
 
-  public boolean equals(Object o) {
-      if (o instanceof RequestedObject) {
-          RequestedObject other = (RequestedObject)o;
-          return this.getName().equals(other.getName());
-      }
-      else {
-          return false;
-      }
-  }
+    public int getSize() {
+        return this.size;
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof RequestedObject) {
+            RequestedObject other = (RequestedObject)o;
+            return this.getName().equals(other.getName());
+        }
+        else {
+            return false;
+        }
+    }
 
 }
