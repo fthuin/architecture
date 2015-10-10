69d868731b74285dc83ee359f1606d49519ef43f
= <florian.thuin@student.uclouvain.be>
2015-10-10 22:17:22 +0200

    Comments on cache LFU

--- src/cache/CacheLFU.java
+++ src/cache/CacheLFU.java
@@ -2,57 +2,62 @@ package cache;
 
 import java.util.HashMap;
 
+/* This class contains the implementation of a LFU cache limited in a
+ * number of slots. Here we consider one resource = one slot, the size of a
+ * resource has no matter.
+ */
+
 public class CacheLFU extends CacheManager {
 	private HashMap<String,RequestedObject> hashmap;
 	private int capacity;
 
-	public CacheLFU(HashMap<String,RequestedObject> hashmap,int capacity)
-	{
-		this.hashmap = hashmap;
-		this.capacity = capacity;
-	}
-
-  public CacheLFU(int capacity)
-  {
-    this.hashmap = new HashMap<String,RequestedObject>(capacity);
-    this.capacity = capacity;
-  }
-
-	public void put(RequestedObject requestObject)
-	{
-		if(this.hashmap.size()>= this.capacity)
-		{
-			remove();
-		}
-    hashmap.put(requestObject.getName(),requestObject);
-	}
-
-	private void remove() {
-    RequestedObject current = null;
-    int min = Integer.MAX_VALUE;
-    for(RequestedObject request : this.hashmap.values())
-    {
-      if(request.getAccessCounter() < min)
-      {
-        min = request.getAccessCounter();
-        current = request;
-      }
+    /* Constructor
+     * @argument : capacity is the maximum number of slots available for
+     * the LFU cache.
+     */
+    public CacheLFU(int capacity) {
+        this.hashmap = new HashMap<String,RequestedObject>(capacity);
+        this.capacity = capacity;
     }
-    hashmap.remove(current.getName());
-	}
 
-	public boolean get(RequestedObject requestObject)
-	{
-    boolean res = true;
-    RequestedObject request = this.hashmap.get(requestObject.getName());
-		if(request==null)
-		{
-      put(requestObject);
-      res = false;
+    /* This method makes room to add a RequestedObject in our data structure,
+     * removing elements if needed.
+     */
+	public void put(RequestedObject requestObject) {
+		if(this.hashmap.size()>= this.capacity) {
+			this.remove();
 		}
-    else
-      request.incrementCounter();
-    return res;
+        hashmap.put(requestObject.getName(),requestObject);
 	}
 
+    /* This method removes the element with the least frequency usage
+     * contained in the data structure.
+     */
+    private void remove() {
+        RequestedObject current = null;
+        int min = Integer.MAX_VALUE;
+        for(RequestedObject request : this.hashmap.values()) {
+            if(request.getAccessCounter() < min) {
+                min = request.getAccessCounter();
+                current = request;
+            }
+        }
+        hashmap.remove(current.getName());
+	}
+
+    /* This method returns true if the RequestedObject was already in the
+     * cache, otherwise it returns false and put the RequestedObject in the
+     * LFU cache.
+     */
+	public boolean get(RequestedObject requestObject){
+        boolean res = true;
+        RequestedObject request = this.hashmap.get(requestObject.getName());
+        if(request==null) {
+            put(requestObject);
+            res = false;
+		} else {
+            request.incrementCounter();
+        }
+        return res;
+	}
 }
