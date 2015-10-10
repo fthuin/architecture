59c310c380695c0ab260542726d5cbfe64c30c2b
gorby_kabasele <gorby_kabasele@student.uclouvain.be>
2015-10-10 16:53:12 +0200

    cache LRU implementation

---
+++ src/cache/CacheLRU.java
@@ -0,0 +1,36 @@
+package cache;
+import java.util.LinkedList;
+
+public class CacheLRU extends CacheManager{
+  private LinkedList<RequestedObject> linkedlist;
+  private int capacity;
+
+  public CacheLRU(int capacity)
+  {
+    this.linkedlist = new LinkedList<RequestedObject>();
+    this.capacity = capacity;
+  }
+  
+  public void put(RequestedObject requestObject)
+  {
+    if(this.linkedlist.size()<=capacity)
+    {
+      linkedlist.removeFirst();
+    }
+    linkedlist.addLast(requestObject);
+  }
+
+
+  public boolean get(RequestedObject requestObject)
+  {
+    boolean res = false;
+    if(this.linkedlist.contains(requestObject)) 
+    {
+      this.linkedlist.remove(requestObject);
+      res = true; 
+    }
+    put(requestObject);
+    return res;
+  }
+  
+}
