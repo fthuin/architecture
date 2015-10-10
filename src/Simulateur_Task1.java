13925659d6ff1c7816c1f295faf695834c52ad1a
= <florian.thuin@student.uclouvain.be>
2015-10-10 22:35:52 +0200

    Séparation des simulations Task1/Task2

---
+++ src/Simulateur_Task1.java
@@ -0,0 +1,46 @@
+import cache.*;
+
+public class Simulateur_Task1 {
+    private int warmupLength;
+    private int slotsNbr;
+    private TraceParser tp = null;
+
+    public Simulateur_Task1(String[] args) {
+        this.warmupLength = Integer.parseInt(args[0]);
+        this.slotsNbr = Integer.parseInt(args[1]);
+        this.tp = new TraceParser(System.in);
+        this.simulate();
+    }
+
+    public void simulate() {
+        CacheLFU cacheLFU = new CacheLFU(slotsNbr);
+        CacheLRU cacheLRU = new CacheLRU(slotsNbr);
+
+        for (int i = 0 ; i < warmupLength ; i++) {
+            RequestedObject ro = tp.getResFromFile();
+            if (ro == null) break;
+            cacheLFU.get(ro);
+            cacheLRU.get(ro);
+        }
+        RequestedObject ro = null;
+        int hitsLFU = 0;
+        int compteur = 0;
+        int hitsLRU = 0;
+
+        while((ro = tp.getResFromFile()) != null) {
+            if (cacheLFU.get(ro)) {
+                hitsLFU++;
+            }
+            if (cacheLRU.get(ro)) {
+                hitsLRU++;
+            }
+            compteur++;
+        }
+        double hitrateLFU = (double)hitsLFU / (double)compteur;
+        double hitrateLRU = (double)hitsLRU / (double)compteur;
+        System.out.println("Hits rate LFU : " + hitrateLFU*100 + "%");
+        System.out.println("Hits rate LRU : " + hitrateLRU*100 + "%");
+        System.out.println("Total requests : " + compteur);
+    }
+
+}
--- src/Simulateur_Task2.java
+++ src/Simulateur_Task2.java
@@ -1,11 +1,11 @@
 import cache.*;
 
-public class Simulateur {
+public class Simulateur_Task2 {
     private int warmupLength;
     private int slotsNbr;
     private TraceParser tp = null;
 
-    public Simulateur(String[] args) {
+    public Simulateur_Task2(String[] args) {
         this.warmupLength = Integer.parseInt(args[0]);
         this.slotsNbr = Integer.parseInt(args[1]);
         this.tp = new TraceParser(System.in);
@@ -13,33 +13,21 @@ public class Simulateur {
     }
 
     public void simulate() {
-        //CacheLFU cacheLFU = new CacheLFU(slotsNbr);
-        //CacheLRU cacheLRU = new CacheLRU(slotsNbr);
         CacheLFUbytes cacheLFUbytes = new CacheLFUbytes(slotsNbr);
         CacheLRUbytes cacheLRUbytes = new CacheLRUbytes(slotsNbr);
 
         for (int i = 0 ; i < warmupLength ; i++) {
             RequestedObject ro = tp.getResFromFile();
             if (ro == null) break;
-            //cacheLFU.get(ro);
-            //cacheLRU.get(ro);
             cacheLFUbytes.get(ro);
             cacheLRUbytes.get(ro);
         }
         RequestedObject ro = null;
-        int hitsLFU = 0;
         int compteur = 0;
-        int hitsLRU = 0;
         int hitsLFUbytes = 0;
         int hitsLRUbytes = 0;
 
         while((ro = tp.getResFromFile()) != null) {
-            /*if (cacheLFU.get(ro)) {
-                hitsLFU++;
-            }
-            if (cacheLRU.get(ro)) {
-                hitsLRU++;
-            }*/
             if (cacheLFUbytes.get(ro)) {
             	hitsLFUbytes++;
             }
@@ -48,12 +36,8 @@ public class Simulateur {
             }
             compteur++;
         }
-        double hitrateLFU = (double)hitsLFU / (double)compteur;
-        double hitrateLRU = (double)hitsLRU / (double)compteur;
         double hitrateLFUbytes = (double)hitsLFUbytes / (double)compteur;
         double hitrateLRUbytes = (double)hitsLRUbytes / (double)compteur;
-        System.out.println("Hits rate LFU : " + hitrateLFU*100 + "%");
-        System.out.println("Hits rate LRU : " + hitrateLRU*100 + "%");
         System.out.println("Hits rate LFUbytes : " + hitrateLFUbytes*100 + "%");
         System.out.println("Hits rate LRUbytes : " + hitrateLRUbytes*100 + "%");
         System.out.println("Total requests : " + compteur);
