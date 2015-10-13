78f942fbb918c4c759f9e870d9ee8543c21632cc
Florian Thuin <florian.thuin@student.uclouvain.be>
2015-10-13 09:19:04 +0200

    Mise a jour simulation en 2 classes pour remise

---
+++ src/Simulation_Task1.java
@@ -0,0 +1,5 @@
+public class Simulation_Task1 {
+    public static void main(String[] args) {
+        new Simulateur_Task1(args);
+    }
+}
--- src/Simulation_Task2.java
+++ src/Simulation_Task2.java
@@ -1,4 +1,4 @@
-public class Simulation {
+public class Simulation_Task2 {
     public static void main(String[] args) {
         new Simulateur_Task2(args);
     }
